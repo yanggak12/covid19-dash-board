aad8d15ec6d863417ac0ece2807eee50ce7c20ca
yanggak12 <yanggak12@naver.com>
2022-01-26 18:09:53 +0900

    chore : east asia data

---
+++ src/utils/countryInfo.ts
@@ -0,0 +1,32 @@
+export const countryData = [
+  {
+    code: 'kr',
+    kName: '한국',
+    eName: 'KOREA',
+  },
+  {
+    code: 'jp',
+    kName: '일본',
+    eName: 'JAPAN',
+  },
+  {
+    code: 'cn',
+    kName: '중국',
+    eName: 'CHINA',
+  },
+  {
+    code: 'tw',
+    kName: '대만',
+    eName: 'TAIWAN',
+  },
+  {
+    code: 'mo',
+    kName: '마카오',
+    eName: 'MACAO',
+  },
+  {
+    code: 'mn',
+    kName: '몽골',
+    eName: 'MONGOLIA',
+  },
+];
